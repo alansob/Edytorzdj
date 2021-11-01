615d8a10b4bc556ccc7b12e003335dd098069ec8
alansob <Asobczak020304@gmail.com>
2021-11-01 14:30:39 +0100

    przechwycenie kamery działa

--- app/src/main/java/com/example/edytorzdjec/MainActivity.java
+++ app/src/main/java/com/example/edytorzdjec/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.edytorzdjec;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.ImageButton;
@@ -32,6 +40,13 @@ public class MainActivity extends AppCompatActivity {
         imgBtn3 = (ImageButton) findViewById(R.id.camera);
         imgBtn4 = (ImageButton) findViewById(R.id.clear);
         imgView = (ImageView) findViewById(R.id.imageView);
+
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    Manifest.permission.CAMERA
+            },100);
+        }
 
         // int picture = 0;
        // int currentPicture = 0;
@@ -81,5 +96,22 @@ public class MainActivity extends AppCompatActivity {
                 imgView.setImageResource(images2[currentPicture]);
             }
         });
+
+        imgBtn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 100);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 100){
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            imgView.setImageBitmap(bitmap);
+        }
     }
 }
